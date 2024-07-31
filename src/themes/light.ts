ed291fe652abb316a2dbab37d268faaf9be9e29a
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-07-31 22:38:21 +0900

    コミットなどのボタンダークモードでのみ暗くした

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -1,5 +1,5 @@
-import {VscodePalette} from '../types/vscode-palette';
-import {VscodeTheme} from '../types/vscode-theme';
+import { VscodePalette } from '../types/vscode-palette';
+import { VscodeTheme } from '../types/vscode-theme';
 import * as Dark from './dark';
 
 export function create(palette: VscodePalette): VscodeTheme {
@@ -7,6 +7,8 @@ export function create(palette: VscodePalette): VscodeTheme {
 	const t = Dark.create(p);
 
 	t.name = 'Iceberg Light';
+	t.colors['button.background'] = p.editor.fg;
+	t.colors['button.hoverBackground'] = p.ansi.brightWhite;
 	t.colors['editor.wordHighlightBackground'] = p.colors.blue.withAlpha(0.15);
 	t.colors['editor.wordHighlightStrongBackground'] = p.colors.blue.withAlpha(
 		0.3,
